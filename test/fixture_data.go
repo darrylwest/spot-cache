eedeb4324fa1cf244d3ffb72b40a2714be7e8831
darryl west <darryl.west@RainCitySoftware.com>
2017-03-12 12:28:34 -0700

    added ulid to test fixtures

--- test/fixture_data.go
+++ test/fixture_data.go
@@ -1,22 +1,50 @@
+/**
+ * test fixtures; should move this to it's own repo for random-utils or something
+ *
+ * @author darryl.west <darryl.west@raincitysoftware.com>
+ * @created 2017-03-12 11:39:18
+ */
+
 package test
 
 import (
     "fmt"
     "math/rand"
     "time"
+	"io"
+	"github.com/oklog/ulid"
 )
 
-var random = rand.New(rand.NewSource(time.Now().UnixNano()))
+var entropy io.Reader = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func genulid(entropy io.Reader, ts uint64) (ulid.ULID, error) {
+	value, err := ulid.New(ts, entropy)
+	return value, err
+}
+
+func CreateRawULID() ulid.ULID {
+	ts := uint64(time.Now().UnixNano() / 1000000)
+    v, _ := genulid(entropy, ts)
+
+    return v
+}
+
+/**
+ * generate and return a ulid as a string
+ */
+func CreateULID() string {
+    return CreateRawULID().String()
+}
 
 func CreateRandomId() string {
-
-    return fmt.Sprintf("%x%x", random.Intn(9e7)+1e8, random.Intn(9e7)+1e8)
+    r := rand.New(rand.NewSource(time.Now().UnixNano()))
+    return fmt.Sprintf("%x%x", r.Intn(9e7)+1e8, r.Intn(9e7)+1e8)
 }
 
+/**
+ * create a command id (ulid)
+ */
 func CreateCommandId() []byte {
-    id := []byte(CreateRandomId())
-
-    return id
+    return []byte(CreateULID())
 }
-
 
