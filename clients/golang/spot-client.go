3e39e9dc2f4402d3e916749fbfd360ec44d16f71
darryl west <darryl.west@RainCitySoftware.com>
2017-03-29 17:00:22 -0700

    added clients section to develop gl, node and python client applications...

---
+++ clients/golang/spot-client.go
@@ -0,0 +1,17 @@
+//
+// Client application for spot-cache
+//
+// @author darryl.west <darryl.west@raincitysoftware.com>
+// @created 2017-03-29 16:58:10
+//
+
+package main
+
+import (
+    "fmt"
+    // _ "spotcache"
+)
+
+func main() {
+    fmt.Println("hello...")
+}
